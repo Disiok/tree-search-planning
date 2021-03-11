9eb58aed929c4d8e6a32337c69fb00f5fba2544a
Kelvin Wong <kelvinwong@cs.toronto.edu>
2021-03-11 00:17:21 -0500

    Bump up # of workers

--- muzero-general/games/highway_env_az.py
+++ muzero-general/games/highway_env_az.py
@@ -32,10 +32,9 @@ class MuZeroConfig:
 
 
         ### Self-Play
-        self.num_workers = 1  # Number of simultaneous threads/workers self-playing to feed the replay buffer
+        self.num_workers = 64 # Number of simultaneous threads/workers self-playing to feed the replay buffer
         self.selfplay_on_gpu = False
         self.max_moves = 500  # Maximum number of moves if game is not finished before
-        # self.num_simulations = 50  # Number of future moves self-simulated
         self.num_simulations = 50  # Number of future moves self-simulated
         self.discount = 0.997  # Chronological discount of the reward
         self.temperature_threshold = None  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
--- muzero-general/muzero.py
+++ muzero-general/muzero.py
@@ -88,7 +88,7 @@ class MuZero:
         if 1 < self.num_gpus:
             self.num_gpus = math.floor(self.num_gpus)
 
-        ray.init(_temp_dir='/scratch/gobi1/sergio/tmp', num_gpus=total_gpus, ignore_reinit_error=True)
+        ray.init(_temp_dir='/scratch/ssd001/home/kelvin/tmp', num_gpus=total_gpus, ignore_reinit_error=True)
 
         # Checkpoint and replay buffer used to initialize workers
         self.checkpoint = {
